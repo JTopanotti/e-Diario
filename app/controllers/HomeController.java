c4eab7d35f5ba26ce28aeb199ffa99570f59c53f
Jonathan Muttini Topanotti <jonathan.topanotti@ei.esmalglass-itaca.com>
2017-09-28 13:00:22 -0300

    Login/Logout funcionando

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -21,21 +21,26 @@ public class HomeController extends Controller {
 	FormFactory criadorFormulario;
 
     public Result index() {
-        return ok(views.html.index.render("Indice", false, null));
+        return ok(views.html.index.render("Indice", Autenticacao.isLoggedIn(ctx()),  
+        		                            Autenticacao.getInfoUsuario(ctx())));
     }
     
     public Result login() {
         InfoUsuarioDB.addUsuario("jonathan", "senha");
     	formularioLogin = criadorFormulario.form(UsuarioLogin.class);
-    	return ok(views.html.login.render("Login", false, null, formularioLogin));
+    	return ok(views.html.login.render("Login", Autenticacao.isLoggedIn(ctx()),
+    			                            Autenticacao.getInfoUsuario(ctx()), formularioLogin));
     }
     
     public Result logout() {
-        return ok(views.html.index.render("Indice", false, null));
+		session().clear();
+        return ok(views.html.index.render("Home", Autenticacao.isLoggedIn(ctx()),  
+        		                            Autenticacao.getInfoUsuario(ctx())));
     }
     
     public Result perfil() {
-    	return ok(views.html.index.render("PERFIL", false, null));
+    	return ok(views.html.index.render("PERFIL", Autenticacao.isLoggedIn(ctx()), 
+    			                            Autenticacao.getInfoUsuario(ctx())));
     }
     
     public Result postLogin() {
@@ -43,11 +48,12 @@ public class HomeController extends Controller {
     	
     	if(formularioLogin.hasErrors()) {
         	System.out.println("Has Errors");
-    		//flash("erro", "Credenciais de login invalidos");
-    		return badRequest(views.html.login.render("Login", false, null, formularioLogin));
+    		flash("erro", "Credenciais de login invalidos");
+    		return badRequest(views.html.login.render("Login", Autenticacao.isLoggedIn(ctx()),  
+    				                                    Autenticacao.getInfoUsuario(ctx()), formularioLogin));
     	} else {
- /*   		session().clear();
-    		session("usuario", formularioLogin.get().getUsuario()); */
+    		session().clear();
+    		session("usuario", formularioLogin.get().getUsuario()); 
     		return redirect(routes.HomeController.perfil());
     	}
     	
