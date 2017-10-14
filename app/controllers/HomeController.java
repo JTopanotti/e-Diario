81691af83c7a7b533d90342dd1c483bb530583dc
Jonathan Muttini Topanotti <jonathan.topanotti@ei.esmalglass-itaca.com>
2017-10-14 03:02:15 -0300

    Versão 1.0

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -33,16 +33,6 @@ public class HomeController extends Controller {
 		conexaoPostgres = Databases.createFrom("playdb", "org.postgresql.Driver", 
 				            "jdbc:postgresql://127.0.0.1:5432/playdb?user=jonathan&password=j09o12n43")
 							.getConnection();
-		try {
-			Statement execucao = conexaoPostgres.createStatement();
-			String query = "SELECT * FROM usuarios";
-			ResultSet resultados = execucao.executeQuery(query);
-			while(resultados.next()) {
-				System.out.println("Dados: " + resultados.getString(1) + " " + resultados.getString(2));
-			}
-		} catch(SQLException e) {
-			System.err.println("Error SQL: " + e);
-		}
 		 
 	}	
 
@@ -52,7 +42,7 @@ public class HomeController extends Controller {
     }
     
     public Result login() {
-        InfoUsuarioDB.addUsuario("jonathan", "senha");
+        InfoUsuarioDB.addUsuario("jonas", "j09o12n43");
     	formularioLogin = criadorFormulario.form(UsuarioLogin.class);
     	return ok(views.html.login.render("Login", Autenticacao.isLoggedIn(ctx()),
     			                            Autenticacao.getInfoUsuario(ctx()), formularioLogin));
@@ -65,8 +55,35 @@ public class HomeController extends Controller {
     }
     
     public Result perfil() {
-    	return ok(views.html.profile.render("Perfil", Autenticacao.isLoggedIn(ctx()),
-    			                            Autenticacao.getInfoUsuario(ctx())));
+    	ResultSet resultados;
+    	int id = 0, numero = 0;
+    	String nome = ".", endereco = ".", bairro = ".", observacoes = ".";
+    	Float[] notas = {new Float(0), new Float(0), new Float(0)};
+    	try {
+			Statement execucao = conexaoPostgres.createStatement();
+			String query = "select * from public.alunos where username = '" + Autenticacao.getUser(ctx()) + "';";
+			resultados = execucao.executeQuery(query);
+			if(resultados.next()) {
+				id = resultados.getShort(1);
+				nome = resultados.getString(2);
+				endereco = resultados.getString(3);
+				numero = resultados.getInt(4);
+				bairro = resultados.getString(5);
+			observacoes = resultados.getString(6);
+			}
+			query = "select * from public.notas where id_aluno = " + id + ";";
+			resultados = execucao.executeQuery(query);
+			if(resultados.next()) {
+				notas[0] = resultados.getFloat(3);
+				notas[1] = resultados.getFloat(2);
+				notas[2] = resultados.getFloat(5);
+			}
+		    } catch(SQLException e) {
+		    	System.err.println("Error SQL: " + e);
+		    }
+		return ok(views.html.profile.render("Perfil", Autenticacao.isLoggedIn(ctx()),
+		                                     Autenticacao.getInfoUsuario(ctx()),
+		                                     nome, endereco, numero, bairro, notas));
     }
     
     public Result postLogin() {
